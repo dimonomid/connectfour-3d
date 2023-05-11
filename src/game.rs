0eeeda8509cdbf822dadf06a9a8224ea0269c59a
Dmitrii Frank <mail@dmitryfrank.com>
2023-05-11 19:58:41 +0300

    Game can check for a winner

--- src/game.rs
+++ src/game.rs
@@ -1,10 +1,18 @@
 use anyhow::{anyhow, Result};
 
-// In "Connect Four", ROW_SIZE is the "Four".
+// In "Connect Four", ROW_SIZE is the "Four". It can be changed to any positive number, and the app
+// will work. Might even make it a parameter, but I didn't bother.
 pub const ROW_SIZE: usize = 4;
 
 pub struct Game {
     board: BoardState,
+
+    win_row: Option<WinRow>,
+}
+
+pub struct WinRow {
+    pub side: Side,
+    pub row: [CoordsFull; ROW_SIZE],
 }
 
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
@@ -49,16 +57,24 @@ impl Game {
     pub fn new() -> Game {
         Game {
             board: BoardState::new(),
+            win_row: None,
         }
     }
 
     pub fn put_token(&mut self, side: Side, x: usize, z: usize) -> Result<PutResult> {
         panic_if_out_of_bounds(x, 0, z);
 
+        // Make sure there is no winner yet.
+        if let Some(win_row) = &self.win_row {
+            return Err(anyhow!("there is a winner already"))
+        }
+
         for y in 0..ROW_SIZE {
             match self.board.get(x, y, z) {
                 None => {
                     self.board.set(side, x, y, z);
+                    self.win_row = self.check_win();
+
                     return Ok(PutResult {
                         y: y,
                         won: false, // TODO
@@ -84,6 +100,165 @@ impl Game {
 
     pub fn reset_board(&mut self, board: &BoardState) {
         self.board.copy_from(board);
+        self.win_row = self.check_win();
+    }
+
+    fn check_win(&self) -> Option<WinRow> {
+        // Vertical rows (constant x, z).
+        for x in 0..ROW_SIZE {
+            for z in 0..ROW_SIZE {
+                let row = self.check_win_row(|y| -> CoordsFull {
+                    return CoordsFull{x, y, z};
+                });
+                if let Some(win_row) = row {
+                    return Some(win_row);
+                }
+            }
+        }
+
+        // Horizontal rows with constant x, y.
+        for x in 0..ROW_SIZE {
+            for y in 0..ROW_SIZE {
+                let row = self.check_win_row(|z| -> CoordsFull {
+                    return CoordsFull{x, y, z};
+                });
+                if let Some(win_row) = row {
+                    return Some(win_row);
+                }
+            }
+        }
+
+        // Horizontal rows with constant z, y.
+        for z in 0..ROW_SIZE {
+            for y in 0..ROW_SIZE {
+                let row = self.check_win_row(|x| -> CoordsFull {
+                    return CoordsFull{x, y, z};
+                });
+                if let Some(win_row) = row {
+                    return Some(win_row);
+                }
+            }
+        }
+
+        // Diagonal rows with constant x.
+        for x in 0..ROW_SIZE {
+            // Ascending y
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x, y: n, z: n};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+
+            // Descending y
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x, y: ROW_SIZE-1-n, z: n};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+        }
+
+        // Diagonal rows with constant z.
+        for z in 0..ROW_SIZE {
+            // Ascending y
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x: n, y: n, z};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+
+            // Descending y
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x: n, y: ROW_SIZE-1-n, z};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+        }
+
+        // Diagonal rows with constant y.
+        for y in 0..ROW_SIZE {
+            // Ascending z
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x: n, y, z: n};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+
+            // Descending z
+            let row = self.check_win_row(|n| -> CoordsFull {
+                return CoordsFull{x: n, y, z: ROW_SIZE-1-n};
+            });
+            if let Some(win_row) = row {
+                return Some(win_row);
+            }
+        }
+
+        // 3D diagonal with ascending x, y, z
+        let row = self.check_win_row(|n| -> CoordsFull {
+            return CoordsFull{x: n, y: n, z: n};
+        });
+        if let Some(win_row) = row {
+            return Some(win_row);
+        }
+
+        // 3D diagonal with ascending x, z; descending y
+        let row = self.check_win_row(|n| -> CoordsFull {
+            return CoordsFull{x: n, y: ROW_SIZE-1-n, z: n};
+        });
+        if let Some(win_row) = row {
+            return Some(win_row);
+        }
+
+        // 3D diagonal with ascending x, y; descending z
+        let row = self.check_win_row(|n| -> CoordsFull {
+            return CoordsFull{x: n, y: n, z: ROW_SIZE-1-n};
+        });
+        if let Some(win_row) = row {
+            return Some(win_row);
+        }
+
+        // 3D diagonal with ascending x; descending y, z
+        let row = self.check_win_row(|n| -> CoordsFull {
+            return CoordsFull{x: n, y: ROW_SIZE-1-n, z: ROW_SIZE-1-n};
+        });
+        if let Some(win_row) = row {
+            return Some(win_row);
+        }
+
+        None
+    }
+
+    fn check_win_row(&self, getter: impl Fn(usize) -> CoordsFull) -> Option<WinRow> {
+        let mut row_side: Option<Side> = None;
+        let mut row = [CoordsFull{x: 0, y: 0, z: 0}; ROW_SIZE];
+
+        for i in 0..ROW_SIZE {
+            let coords = getter(i);
+            match self.get_token(coords.x, coords.y, coords.z) {
+                Some(side) => {
+                    if i == 0 {
+                        row_side = Some(side);
+                        continue;
+                    }
+
+                    if row_side != Some(side) {
+                        return None;
+                    }
+                },
+
+                None => { return None; }
+            }
+        }
+
+        // Winning row!
+        Some(WinRow{
+            side: row_side.unwrap(),
+            row: row,
+        })
     }
 }
 
